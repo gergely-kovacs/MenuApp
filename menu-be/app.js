175ee211af26412d39215c12ce100e1f3f73f669
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-04-21 15:51:22 +0200

    menu backend initial commit

---
+++ menu-be/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import { join } from "node:path";
+import AutoLoad from "@fastify/autoload";
+
+// Pass --options via CLI arguments in command to enable these options.
+const options = {};
+
+export default async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+}
+
+const _options = options;
+export { _options as options };
