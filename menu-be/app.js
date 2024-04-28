3e53eea625a576b47ae85692cbf88588e6a06d2c
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-04-28 19:29:45 +0200

    reverted the backend to commonjs module

--- menu-be/app.js
+++ menu-be/app.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import { join } from "node:path";
-import AutoLoad from "@fastify/autoload";
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
 
 // Pass --options via CLI arguments in command to enable these options.
 const options = {};
 
-export default async function (fastify, opts) {
+module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
   // Do not touch the following lines
@@ -15,17 +15,16 @@ export default async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: join(__dirname, "plugins"),
+    dir: path.join(__dirname, "plugins"),
     options: Object.assign({}, opts),
   });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: join(__dirname, "routes"),
+    dir: path.join(__dirname, "routes"),
     options: Object.assign({}, opts),
   });
-}
+};
 
-const _options = options;
-export { _options as options };
+module.exports.options = options;
