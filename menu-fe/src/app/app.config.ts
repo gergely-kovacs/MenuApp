2eeb0b779aec277be5f06351319c5fe84b335dda
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-04-28 19:30:14 +0200

    added alternative sizes for images

--- menu-fe/src/app/app.config.ts
+++ menu-fe/src/app/app.config.ts
@@ -1,28 +1,28 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient } from "@angular/common/http";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { routes } from "./app.routes";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), { provide: LOCALE_ID, useValue: 'hu-HU' },
-  provideHttpClient(),
+  providers: [
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: "hu-HU" },
+    provideHttpClient(),
     importProvidersFrom(
-      TranslateModule.forRoot(
-        {
-          loader: {
-              provide: TranslateLoader,
-              useFactory: HttpLoaderFactory,
-              deps: [HttpClient]
-          }
-      }
-      )
-    )
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 };
--- menu-fe/src/app/menu-page/menu-item/menu-item.component.ts
+++ menu-fe/src/app/menu-page/menu-item/menu-item.component.ts
@@ -1,22 +1,22 @@
-import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { Recipe } from '../menu-backend.service';
+import { CurrencyPipe } from "@angular/common";
+import { Component, input } from "@angular/core";
+import { Recipe } from "../menu-backend.service";
 
 @Component({
-  selector: 'app-menu-item',
+  selector: "app-menu-item",
   standalone: true,
-  imports: [CurrencyPipe, NgOptimizedImage],
+  imports: [CurrencyPipe],
   template: `
     <div class="min-w-[160px] h-full border-gray-600 border-2 rounded-md p-4">
       <h3>{{ recipe().name }}</h3>
-      <div>{{ recipe().price | currency : 'HUF' }}</div>
+      <div>{{ recipe().price | currency : "HUF" }}</div>
       <div class="flex flex-wrap gap-2">
         @if (recipe().glutenFree) {
         <img
           width="64"
           height="64"
-          ngSrc="assets/gluten-free-icon.jpg"
-          ngSrcset="128w"
+          srcset="assets/gluten-free-icon-128w.jpg 128w, assets/gluten-free-icon.jpg 3969w"
+          sizes="64px"
           alt="Gluten-free"
           priority
         />
@@ -24,8 +24,8 @@ import { Recipe } from '../menu-backend.service';
         <img
           width="64"
           height="64"
-          ngSrc="assets/lactose-free-icon.jpg"
-          ngSrcset="128w"
+          srcset="assets/lactose-free-icon-128w.jpg 128w, assets/lactose-free-icon.jpg 490w"
+          sizes="64px"
           alt="Lactose-free"
           priority
         />
